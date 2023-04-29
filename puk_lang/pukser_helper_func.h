f84aa2a782bbcbe7a480e01a7ecee176cacbe518
Mishelin69 <miloskosec90@gmail.com>
2023-04-29 21:24:10 +0200

    puk_test

--- puk_lang/pukser_helper_func.h
+++ puk_lang/pukser_helper_func.h
@@ -2,44 +2,6 @@
 
 #include <stdint.h>
 
-int32_t str_toi32(const std::string& s) {
+int32_t str_toi32(const std::string& s);
 
-	size_t strt = 0;
-	int32_t v = 0; 
-	uint32_t mul = 1;
-	bool negative = false;
-
-	if (s[0] == '-') {
-		negative = true;
-		strt = 1;
-	}
-
-	for (strt; strt < s.length(); ++strt) {
-		v *= mul;
-		v += s[strt] - '0';
-		mul *= 10;
-	}
-
-	return v;
-}
-
-int64_t str_toi64(const std::string& s) {
-
-	size_t strt = 0;
-	int64_t v = 0;
-	uint32_t mul = 1;
-	bool negative = false;
-
-	if (s[0] == '-') {
-		negative = true;
-		strt = 1;
-	}
-
-	for (strt; strt < s.length(); ++strt) {
-		v *= mul;
-		v += s[strt] - '0';
-		mul *= 10;
-	}
-
-	return v;
-}
+int64_t str_toi64(const std::string& s);
