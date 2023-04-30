9920c9a77cef082c47f56d93f498b377bf6287d4
Mishelin69 <kosecmilos6@gmail.com>
2023-04-30 14:08:31 +0200

    puk puk

--- puk_lang/pukser_helper_func.cpp
+++ puk_lang/pukser_helper_func.cpp
@@ -7,12 +7,6 @@ int32_t str_toi32(const std::string& s) {
 	size_t strt = 0;
 	int32_t v = 0;
 	uint32_t mul = 1;
-	bool negative = false;
-
-	if (s[0] == '-') {
-		negative = true;
-		strt = 1;
-	}
 
 	for (; strt < s.length(); ++strt) {
 		v *= mul;
@@ -28,12 +22,6 @@ int64_t str_toi64(const std::string& s) {
 	size_t strt = 0;
 	int64_t v = 0;
 	uint32_t mul = 1;
-	bool negative = false;
-
-	if (s[0] == '-') {
-		negative = true;
-		strt = 1;
-	}
 
 	for (; strt < s.length(); ++strt) {
 		v *= mul;
@@ -79,6 +67,36 @@ double str_tof64(const std::string& s) {
 
 		v += (double)((c - '0') * pow(10, pn * mul));
 		mul += 1.0;
+	}
+
+	return v;
+}
+
+uint32_t str_tou32(const std::string& s) {
+
+	size_t strt = 0;
+	int32_t v = 0;
+	uint32_t mul = 1;
+
+	for (; strt < s.length(); ++strt) {
+		v *= mul;
+		v += s[strt] - '0';
+		mul *= 10;
+	}
+
+	return v;
+}
+
+uint64_t str_tou64(const std::string& s) {
+
+	size_t strt = 0;
+	int64_t v = 0;
+	uint32_t mul = 1;
+
+	for (; strt < s.length(); ++strt) {
+		v *= mul;
+		v += s[strt] - '0';
+		mul *= 10;
 	}
 
 	return v;
--- puk_lang/pukser_helper_func.h
+++ puk_lang/pukser_helper_func.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstdint>
 #include <stdint.h>
 #include <string>
 
@@ -10,3 +11,7 @@ int64_t str_toi64(const std::string& s);
 float str_tof32(const std::string& s);
 
 double str_tof64(const std::string& s);
+
+uint32_t str_tou32(const std::string& s);
+
+uint64_t str_tou64(const std::string& s);
